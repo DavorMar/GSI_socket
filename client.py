afa267dd597f8c5f4fea303cd8729a2ccb8ff70e
Davor <davor,programming@gmail.com>
2023-05-17 22:47:12 +0200

    added prompts for user input on ip and port numbers, can of GSI file for port number

--- client.py
+++ client.py
@@ -4,16 +4,15 @@ import json
 from datetime import datetime, timedelta
 
 HEADER = 64
-PORT = 5050
+# PORT = 5050
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.56.1"
-ADDR = (SERVER,PORT)
-CHUNK_SIZE = 8192
+# SERVER = "192.168.56.1"
+CHUNK_SIZE = 8192 * 4
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-print("client connected")
+
+
+
 
 
 def send(msg):
@@ -25,31 +24,57 @@ def send(msg):
     client.send(message)
     print((client.recv(2000)))
 
+if __name__ == "__main__":
+    ip_inputted = False
+    while not ip_inputted:
+        ip = input("please input the IP: ")
+        port = input("please input the port: ")
+        while True:
+            correct = input(f"Is your choice correct? [{ip}:{port}] y/n ")
+            if correct.lower() == "y":
+                ip_inputted = True
+                break
+            elif correct.lower() == "n":
+                break
+            else:
+                print("wrong input, please respond with y or n")
 
-try:
-    print("Starting data read")
-    time.sleep(3)
-    while True:
-        date_chunk = client.recv(CHUNK_SIZE).decode(FORMAT)
-        try:
-            time = datetime.strptime(date_chunk,'%Y-%m-%d %H:%M:%S.%f')
-            print(datetime.now() - time)
-        except:
-            pass
+    addr = (ip, int(port))
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(addr)
+    print("client connected")
+    try:
+        print("Starting data read")
+        time.sleep(3)
+        while True:
+            date_chunk = client.recv(CHUNK_SIZE).decode(FORMAT)
+            try:
+                time = datetime.strptime(date_chunk,'%Y-%m-%d %H:%M:%S.%f')
+                # print(datetime.now() - time)
+            except:
+                pass
 
-        received_data = ""
-        loop_bool = True
-        while loop_bool:
-            chunk = client.recv(CHUNK_SIZE).decode(FORMAT)
-            if not chunk:
-                print("not chunk")
-                loop_bool = False
-            elif chunk in ["done", " done"]:
-                loop_bool = False
-            received_data += chunk
-                # print(received_data)
+            received_data = ""
+            loop_bool = True
+            while loop_bool:
+                chunk = client.recv(CHUNK_SIZE).decode(FORMAT)
+                if not chunk:
+                    print("not chunk")
+                    loop_bool = False
+                elif chunk in ["done", " done"]:
+                    loop_bool = False
+                    try:
+                        data = json.loads(received_data)
+                        print(data)
+                    except:
+                        print("failed first json")
+                received_data += chunk
+                    # print(received_data)
+    except KeyboardInterrupt:
+        send(DISCONNECT_MESSAGE)
+        print("Disconnecting")
 
 
-finally:
-    send(DISCONNECT_MESSAGE)
-    print("Disconnecting")
+    finally:
+        send(DISCONNECT_MESSAGE)
+        print("Disconnecting")
--- main_server.py
+++ main_server.py
@@ -4,6 +4,7 @@ import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from datetime import datetime, timedelta
+import re
 
 
 
@@ -14,7 +15,7 @@ ADDR = (SERVER, PORT)
 HEADER = 64
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
-CHUNK_SIZE = 8192
+CHUNK_SIZE = 8192 * 4
 
 
 
@@ -30,6 +31,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.server.payload = [payload[i:i + CHUNK_SIZE] for i in range(0, len(payload), CHUNK_SIZE)]
         self.server.payload.append(now.strftime('%Y-%m-%d %H:%M:%S.%f'))
         self.server.running = True
+        # print(datetime.now()-now)
         # with open(r"data_sample.json", "w") as fp:
         #     json.dump(payload, fp, indent=4)
 
@@ -71,14 +73,16 @@ class Server(HTTPServer):
                 time_string = payload.pop()
                 payload.append("done")
                 payload.append(time_string)
+
                 for chunk in payload:
                     conn.send(chunk.encode(FORMAT))
                     time.sleep(0.0001)
-
+                print(datetime.now() - datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S.%f'))
             except BlockingIOError as e:
                 if e.errno != 10035:
                     # Reraise the exception if it's not the expected error
                     raise
+
         conn.close()
 
 
@@ -109,11 +113,51 @@ class Server(HTTPServer):
                     # Reraise the exception if it's not the expected error
                     raise
 
+def scan_port():
+    path = r"C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_dota2-gsi.cfg"
+    with open(path, "r") as fp:
+        read_string = fp.read()
+
+    pattern = r':\d{4,6}'
+
+    try:
+        port = int(re.search(pattern, read_string).group()[1:])
+        return port
+    except AttributeError:
+        return 12345678
+
+def prompt_port():
+    port = scan_port()
+    if port == 12345678:
+        print("couldnt scan port in GSI cfg file.")
+    else:
+        while True:
+            answer = input(f"Do you want to use scanned port {port}? y/n ")
+            if answer.lower() == "y":
+                return port
+            elif answer.lower() == "n":
+                break
+            else:
+                print("Please write a y/n answer")
+                continue
+    while True:
+        port = input("Please input the port your GSI config file is set to or your desired port: ")
+        try:
+            port = int(port)
+        except ValueError:
+            print("Input a correct port number between 80 and 99999")
+            continue
+        if port < 80 or port > 99999:
+            print("Input a correct port number between 80 and 99999")
+        else:
+            print(f"Server is starting on port {port}")
+            return port
 
 
 
 if __name__ == '__main__':
-    server2 = Server(("0.0.0.0", 3000))
+    port = prompt_port()
+    server2 = Server(("0.0.0.0", port))
     # app.run(host='0.0.0.0', port=3000)
     print("Server is starting")
     server2.start()
