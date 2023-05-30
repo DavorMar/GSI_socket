affa094de0b5e863a22c785c0ea2a79af46f4c51
Davor <davor,programming@gmail.com>
2023-05-30 11:28:53 +0200

    restructured client code to catch errors and be cleaner a bit

--- client.py
+++ client.py
@@ -4,10 +4,11 @@ from datetime import datetime, timedelta
 import time
 
 HEADER = 64
-FORMAT = "utf-8"
+FORMAT = "utf-16"
 DISCONNECT_MESSAGE = "!DISCONNECT"
-CHUNK_SIZE = 32768
+CHUNK_SIZE = 32768 * 2
 TEST = True
+
 
 
 
@@ -43,14 +44,17 @@ if __name__ == "__main__":
 
         print("Starting data read")
         time.sleep(3)
+
         while True:
             try:
-                date_chunk = client.recv(CHUNK_SIZE).decode(FORMAT)
-                try:
-                    timex = datetime.strptime(date_chunk,'%Y-%m-%d %H:%M:%S.%f')
-                    print(datetime.now() - timex)
-                except:
-                    pass
+                if TEST:
+                    date_chunk = client.recv(CHUNK_SIZE).decode(FORMAT)
+                    try:
+                        timex = datetime.strptime(date_chunk,'%Y-%m-%d %H:%M:%S.%f')
+                        print(datetime.now() - timex)
+                        time.sleep(0.01 - (datetime.now() - timex))
+                    except:
+                        pass
 
                 received_data = ""
                 loop_bool = True
@@ -63,6 +67,8 @@ if __name__ == "__main__":
                         loop_bool = False
                         try:
                             data = json.loads(received_data)
+                            with open("dota_data.json", "w") as fp:
+                                json.dump(data, fp, indent=4)
                             # print(data)
                         except:
                             print("failed first json")
--- main_server.py
+++ main_server.py
@@ -7,30 +7,40 @@ from datetime import datetime, timedelta
 
 
 HEADER = 64
-FORMAT = "utf-8"
+FORMAT = "utf-16"
 DISCONNECT_MESSAGE = "!DISCONNECT"
-CHUNK_SIZE = 32768
+CHUNK_SIZE = 32768 * 2
 TEST = True
+
+
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         now = datetime.now()
+        # print(self.client_address)
         length = int(self.headers["Content-Length"])
         body = self.rfile.read(length).decode("utf-8")
         payload = json.loads(body)
+        # if 'map' in payload.keys():
+        #     print(len(json.dumps(payload)), payload['draft'], payload['map']['clock_time'], payload['map']['game_state'], payload['map']['win_team'],payload['auth'])
+        # else:
+        #     print(payload)
         payload = json.dumps(payload)
         self.server.payload = [payload[i:i + CHUNK_SIZE] for i in range(0, len(payload), CHUNK_SIZE)]
+        # print(len(self.server.payload))
+        self.server.running = True
         if TEST:
             self.server.payload.append(now.strftime('%Y-%m-%d %H:%M:%S.%f'))
-            self.server.running = True
+
 
 
 class Server(HTTPServer):
     def __init__(self,server_address, socket_port):
         super(Server, self).__init__(server_address, RequestHandler)
         self.serverx = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ip_addr = socket.gethostbyname(socket.gethostname())
+        # self.ip_addr = socket.gethostbyname(socket.gethostname())
+        self.ip_addr = "192.168.0.14"
         addr = (self.ip_addr, socket_port)
         self.serverx.bind(addr)
         self.serverx.setblocking(False)
@@ -58,16 +68,19 @@ class Server(HTTPServer):
                     raise
 
             try:
+
                 payload = self.payload[:]
+
                 if TEST:
                     time_string = payload.pop()
                 payload.append("done")
                 if TEST:
                     payload.append(time_string)
+                print(len(payload), len(payload[0]))
                 for chunk in payload:
                     conn.send(chunk.encode(FORMAT))
                     time.sleep(0.0001)
-                print(datetime.now() - datetime.strptime(time_string, '%Y-%m-%d %H:%M:%S.%f'))
+
             except BlockingIOError as e:
                 if e.errno != 10035:
                     # Reraise the exception if it's not the expected error
