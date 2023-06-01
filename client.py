a72cfe2f846adf44bca363deeb9c1fc9b9b920ab
Kichma <davor.programming@gmail.com>
2023-06-01 22:40:06 +0200

    client code cleanup, print cleanup

--- client.py
+++ client.py
@@ -6,13 +6,24 @@ import time
 HEADER = 5
 FORMAT = "utf-8 "
 DISCONNECT_MESSAGE = "!DISC"
-CHUNK_SIZE = 1211
+CHUNK_SIZE = 1024
 TEST = False
+TIME_SLEEPING = 0.05
 
 
 def send(msg):
     message = msg.encode(FORMAT)
     client.send(message)
+
+def print_loading(i):
+    if i % 40 == 0:
+        print("\r Fetching data | ", end="")
+    elif i % 40 == 10:
+        print("\r Fetching data / ", end="")
+    elif i % 40 == 20:
+        print("\r Fetching data - ", end="")
+    elif i % 40 == 30:
+        print("\r Fetching data \ ", end="")
 
 if __name__ == "__main__":
     ip_inputted = False
@@ -33,35 +44,41 @@ if __name__ == "__main__":
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(addr)
-        print("client connected")
+        print(f"client connected to {addr[0]}, {addr[1]}")
         print("Starting data read")
         time.sleep(2)
-
+        counter = 0
         while True:
+            print_loading(counter)
+            counter += 1
+            if counter == 120:
+                counter = 0
             try:
+                time.sleep(TIME_SLEEPING)
                 send("!GIVE")
-                msg_len = client.recv(5).decode(FORMAT)
+                msg_len = client.recv(HEADER).decode(FORMAT)
                 msg_len = int(msg_len)
                 bytes_recd = 0
                 whole_data_string = []
                 while bytes_recd < msg_len:
-                    chunk = client.recv(min(msg_len - bytes_recd, 1024))
+                    chunk = client.recv(min(msg_len - bytes_recd, CHUNK_SIZE))
                     if chunk == b'':
-                        raise RuntimeError("socket connection broken")
+                        raise RuntimeError("\nsocket connection broken")
                     whole_data_string.append(chunk)
                     bytes_recd = bytes_recd + len(chunk)
                 whole_data_string = b''.join(whole_data_string)
                 whole_data_json = json.loads(whole_data_string)
-                print(whole_data_json.keys())
-                print("finished")
+
+                if whole_data_json["game_state"]:
+                    with open("gsi_data.json", "w") as fp:
+                        json.dump(whole_data_json, fp=fp, indent=4)
 
             except ValueError:
-                print("value fail")
+                print("\nvalue fail")
 
             except KeyboardInterrupt:
-                print("Interrupting program")
+                print("\nInterrupting program")
                 break
-
     finally:
         send(DISCONNECT_MESSAGE)
         time.sleep(1)
